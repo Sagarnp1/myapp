7a2e518483e863d9cf99957c02d185f4a60f01f1
ayushh-gautam <ayushgautam896@gmail.com>
2025-01-16 12:13:31 +0545

    refractor

--- lib/screens/KitchenScreen.dart
+++ lib/screens/KitchenScreen.dart
@@ -56,79 +56,90 @@ class _KitchenPageState extends State<KitchenPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage(
-                'assets/nightkitchen.jpg',
-              ),
-              fit: BoxFit.cover)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 50),
-          if (!_isConnected) ...[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                controller: _switchboardController,
-                decoration: InputDecoration(
-                  labelText: 'Enter Switchboard API/ID',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: connectToSwitch,
-              child: Text('Connect to Switch'),
-            ),
-          ] else ...[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Connected to Switch: ${_switchboardController.text}',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            for (var device in devices.keys)
-              Card(
-                color: Colors.black.withOpacity(0.6),
-                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                child: ListTile(
-                  title: Text(
-                    device,
-                    style: TextStyle(color: Colors.white),
+        resizeToAvoidBottomInset: false,
+        body: SingleChildScrollView(
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(
+                      'assets/nightkitchen.jpg',
+                    ),
+                    fit: BoxFit.cover)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 50),
+                if (!_isConnected) ...[
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                      controller: _switchboardController,
+                      decoration: InputDecoration(
+                        labelText: 'Enter Switchboard API/ID',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
                   ),
-                  trailing: device == "Fan"
-                      ? DropdownButton<int>(
-                          value: devices[device],
-                          dropdownColor: Colors.black,
-                          items: [
-                            DropdownMenuItem(value: 0, child: Text("Off")),
-                            DropdownMenuItem(value: 1, child: Text("Low")),
-                            DropdownMenuItem(value: 2, child: Text("Medium")),
-                            DropdownMenuItem(value: 3, child: Text("High")),
-                          ],
-                          onChanged: (value) {
-                            setState(() {
-                              devices[device] = value!;
-                            });
-                          },
-                        )
-                      : Switch(
-                          value: devices[device],
-                          onChanged: (value) => toggleDevice(device),
-                          activeColor: Colors.green,
+                  SizedBox(
+                    height: 40,
+                  ),
+                  ElevatedButton(
+                    onPressed: connectToSwitch,
+                    child: Text('Connect to Switch'),
+                  ),
+                ] else ...[
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'Connected to Switch: ${_switchboardController.text}',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  for (var device in devices.keys)
+                    Card(
+                      color: Colors.black.withOpacity(0.6),
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 16),
+                      child: ListTile(
+                        title: Text(
+                          device,
+                          style: TextStyle(color: Colors.white),
                         ),
-                ),
-              ),
-          ],
-        ],
-      ),
-    ));
+                        trailing: device == "Fan"
+                            ? DropdownButton<int>(
+                                value: devices[device],
+                                dropdownColor: Colors.black,
+                                items: [
+                                  DropdownMenuItem(
+                                      value: 0, child: Text("Off")),
+                                  DropdownMenuItem(
+                                      value: 1, child: Text("Low")),
+                                  DropdownMenuItem(
+                                      value: 2, child: Text("Medium")),
+                                  DropdownMenuItem(
+                                      value: 3, child: Text("High")),
+                                ],
+                                onChanged: (value) {
+                                  setState(() {
+                                    devices[device] = value!;
+                                  });
+                                },
+                              )
+                            : Switch(
+                                value: devices[device],
+                                onChanged: (value) => toggleDevice(device),
+                                activeColor: Colors.green,
+                              ),
+                      ),
+                    ),
+                ],
+              ],
+            ),
+          ),
+        ));
   }
 }
